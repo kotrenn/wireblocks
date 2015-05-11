0894e59825dce60d9533fb0aed1a6255fccc21d2
Ramona Seay <kotrenn@gmail.com>
2015-05-10 23:33:23 -0400

    Color choice updates.

--- wireblocks/src/wireblocks/GridPainter.java
+++ wireblocks/src/wireblocks/GridPainter.java
@@ -9,25 +9,25 @@ import java.util.Map;
 public class GridPainter
 {
 	private static final Color[] PALETTE    = { Color.RED, Color.YELLOW,
-	        Color.GREEN, Color.BLUE        };
-	private static final Color   EDGE_COLOR = Color.MAGENTA;
-	
+	                                            Color.GREEN, Color.BLUE        };
+	private static final Color   EDGE_COLOR = Color.magenta;
+
 	Grid                         m_grid;
 	Graph                        m_graph;
 	ArrayList<Integer>           m_buckets;
 	int                          m_numNodes;
 	int                          m_numRegions;
-
+	
 	public GridPainter(Grid grid)
 	{
 		m_grid = grid;
-		
+
 		GraphBuilder builder = new GraphBuilder(m_grid);
 		m_graph = builder.buildGraph();
 		Map<Integer, Integer> regionMap = builder.getRegions();
-
+		
 		System.out.println(builder);
-
+		
 		for (int x = 0; x < m_grid.getWidth(); ++x)
 			for (int y = 0; y < m_grid.getHeight(); ++y)
 			{
@@ -40,32 +40,32 @@ public class GridPainter
 				paintPolyomino(builder, cell, region, dstColor);
 			}
 	}
-	
+
 	public void update()
 	{
 	}
-
+	
 	/* DFS paint can filling algorithm */
 	private void paintPolyomino(GraphBuilder builder, Vector2i root,
 	                            int region, Color dstColor)
 	{
 		Block block = m_grid.getBlock(root);
 		if (block == null) return;
-		
+
 		int index = builder.indexOf(root);
 		Map<Integer, Integer> regionMap = builder.getRegions();
 		if (regionMap.get(index) != region) return;
-
+		
 		if (block.getColor() == dstColor) return;
 		block.setColor(dstColor);
-
+		
 		for (Direction direction : Direction.toArray())
 		{
 			Vector2i vel = direction.toVector();
 			Vector2i newLoc = root.add(vel);
-
+			
 			if (!m_grid.isInBounds(newLoc)) continue;
-
+			
 			paintPolyomino(builder, newLoc, region, dstColor);
 		}
 	}
