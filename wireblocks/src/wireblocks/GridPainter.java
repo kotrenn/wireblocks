a47f60878644141409ad03e5066a33daaafc0fe9
Ramona Seay <kotrenn@gmail.com>
2015-06-03 18:23:47 -0400

    Updated a couple constants for playability.

--- wireblocks/src/wireblocks/GridPainter.java
+++ wireblocks/src/wireblocks/GridPainter.java
@@ -35,15 +35,15 @@ import wireblocks.graphtools.Graph;
 public class GridPainter
 {
 	private static final Color[] PALETTE    = { Color.RED, Color.YELLOW,
-	                                            Color.GREEN, Color.BLUE        };
+	        Color.GREEN, Color.CYAN        };
 	private static final Color   EDGE_COLOR = Color.magenta;
-
+	
 	Grid                         m_grid;
 	GraphBuilder                 m_graphBuilder;
 	Graph                        m_graph;
 	int                          m_numColors;
 	int[]                        m_colors;
-	
+
 	public GridPainter(Grid grid)
 	{
 		m_grid = grid;
@@ -55,12 +55,12 @@ public class GridPainter
 			System.out.print(" " + m_colors[i]);
 		System.out.println("");
 	}
-
+	
 	public void shuffleColors()
-    {
-	    m_colors = RandomUtils.createShuffledArray(m_numColors);
-    }
-
+	{
+		m_colors = RandomUtils.createShuffledArray(m_numColors);
+	}
+	
 	public void clearGrid()
 	{
 		for (int x = 0; x < m_grid.getWidth(); ++x)
@@ -71,48 +71,48 @@ public class GridPainter
 				block.setColor(Block.NO_COLOR);
 			}
 	}
-
+	
 	public void paintGrid()
 	{
 		m_graph = m_graphBuilder.buildGraph();
-		
-		System.out.println(m_graphBuilder);
 
+		System.out.println(m_graphBuilder);
+		
 		System.out.println(m_graph);
 		int[] colors = colorGraph();
-		
+
 		paintCells(m_graphBuilder, colors);
 	}
-	
+
 	private String vertexToString(int v)
 	{
 		return String.valueOf(v);
 	}
-	
+
 	private String edgeToString(int u, int v, int color)
 	{
 		int lower = Math.min(u, v);
 		int upper = Math.max(u, v);
 		return "" + lower + "_" + upper + "_" + color;
 	}
-
+	
 	private String attachColorToString(String label, int color)
 	{
 		return label + "_" + color;
 	}
-
+	
 	private int[] colorGraph()
 	{
 		int numVertices = m_graph.numberOfVertices();
-
+		
 		String[] vertexLabels = new String[numVertices];
-
+		
 		for (int v = 0; v < numVertices; ++v)
 			vertexLabels[v] = vertexToString(v);
-
+		
 		System.out.println("vertexLabels:");
 		System.out.println(vertexLabels);
-
+		
 		List<Vector2i> edges = new LinkedList<Vector2i>();
 		for (int v = 0; v < numVertices; ++v)
 		{
@@ -123,7 +123,7 @@ public class GridPainter
 				edges.add(new Vector2i(u, v));
 			}
 		}
-		
+
 		List<String> edgeLabels = new LinkedList<String>();
 		for (Vector2i edge : edges)
 		{
@@ -135,64 +135,64 @@ public class GridPainter
 				edgeLabels.add(edgeLabel);
 			}
 		}
-
+		
 		System.out.println("edgeLabels:");
 		System.out.println(edgeLabels);
-
+		
 		/* Build our problem instance */
 		SetCoverInstance instance = new SetCoverInstance();
-
+		
 		/* Add *required* vertex columns */
 		for (String vertex : vertexLabels)
 			instance.addColumnLabel(vertex);
-
+		
 		/* Add *optional* edge columns */
 		for (String edge : edgeLabels)
 			instance.addColumnLabel(edge, true);
-		
+
 		/* Add edge coloring constraints */
 		for (int v = 0; v < numVertices; ++v)
 		{
 			List<Integer> neighbors = m_graph.getNeighbors(v);
-
+			
 			for (int i = 0; i < m_numColors; ++i)
 			{
 				int color = m_colors[i];
-
+				
 				/* Construct the name of this row */
 				String rowLabel = attachColorToString(vertexToString(v), color);
-				
+
 				/* Create a list of row constraints */
 				List<String> rowConstraints = new LinkedList<String>();
-				
+
 				/* Add in the requirement of the vertex */
 				rowConstraints.add(vertexToString(v));
-				
+
 				/* Now add in edge coloring constraints */
 				for (int u : neighbors)
 				{
 					String edgeLabel = edgeToString(u, v, color);
 					rowConstraints.add(edgeLabel);
 				}
-
-				System.out.println("row " + rowLabel + ": " + rowConstraints);
 				
+				System.out.println("row " + rowLabel + ": " + rowConstraints);
+
 				/* Finally, add the row to the instance */
 				int numConstraints = rowConstraints.size();
 				String[] constraints = rowConstraints
-						.toArray(new String[numConstraints]);
+				        .toArray(new String[numConstraints]);
 				instance.addRow(rowLabel, constraints);
 			}
 		}
-		
+
 		/* Build our solver */
 		DLXSolver solver = new DLXSolver(instance);
-		
+
 		/* Find a solution and print it out */
 		List<LinkNode> solution = solver.solve();
 		List<String> solutionLabels = instance.solutionToLabels(solution);
 		System.out.println(solutionLabels);
-
+		
 		int[] colorings = new int[numVertices];
 		for (String label : solutionLabels)
 		{
@@ -201,14 +201,14 @@ public class GridPainter
 			int color = Integer.parseInt(vals[1]);
 			colorings[vertex] = color;
 		}
-		
+
 		return colorings;
 	}
-	
+
 	private void paintCells(GraphBuilder builder, int[] colors)
 	{
 		Map<Integer, Integer> regionMap = builder.getRegions();
-		
+
 		for (int x = 0; x < m_grid.getWidth(); ++x)
 			for (int y = 0; y < m_grid.getHeight(); ++y)
 			{
@@ -221,32 +221,32 @@ public class GridPainter
 				paintPolyomino(builder, cell, region, dstColor);
 			}
 	}
-
+	
 	public void update()
 	{
 	}
-	
+
 	/* DFS paint can filling algorithm */
 	private void paintPolyomino(GraphBuilder builder, Vector2i root,
 	                            int region, Color dstColor)
 	{
 		Block block = m_grid.getBlock(root);
 		if (block == null) return;
-
+		
 		int index = builder.indexOf(root);
 		Map<Integer, Integer> regionMap = builder.getRegions();
 		if (regionMap.get(index) != region) return;
-		
+
 		if (block.getColor() == dstColor) return;
 		block.setColor(dstColor);
-		
+
 		for (Direction direction : Direction.toArray())
 		{
 			Vector2i vel = direction.toVector();
 			Vector2i newLoc = root.add(vel);
-			
+
 			if (!m_grid.isInBounds(newLoc)) continue;
-			
+
 			paintPolyomino(builder, newLoc, region, dstColor);
 		}
 	}
--- wireblocks/src/wireblocks/Polyomino.java
+++ wireblocks/src/wireblocks/Polyomino.java
@@ -30,63 +30,63 @@ public class Polyomino
 	{
 		private Vector2i m_loc;
 		private Block    m_block;
-		
+
 		public Cell(Vector2i loc, Block block)
 		{
 			m_loc = loc;
 			m_block = block;
 		}
-		
+
 		public Vector2i getLoc()
 		{
 			return m_loc;
 		}
-		
+
 		public Block getBlock()
 		{
 			return m_block;
 		}
-
+		
 		public void setLoc(Vector2i loc)
 		{
 			m_loc = loc;
 		}
 	}
-
+	
 	private ArrayList<Cell> m_cells;
 	private int             m_offset;
-	
+
 	public Polyomino()
 	{
 		m_cells = new ArrayList<Cell>();
 		m_offset = 0;
-		
+
 		/* Now build */
 		build();
 	}
-	
+
 	private void build()
 	{
 		// int[] sizeDistribution = { 2, 2, 2, 2, 3, 3, 3, 4, 4, 5 };
 		int[] sizeDistribution = { 2, 2, 2, 2, 3 };
 		int size = RandomUtils.randomFromDist(sizeDistribution);
 		// size = 6;
-		
+
 		ArrayList<Vector2i> visited = new ArrayList<Vector2i>();
 		ArrayList<Vector2i> available = new ArrayList<Vector2i>();
-		
+
 		/* Create a root node */
 		available.add(new Vector2i(0, 0));
-		
+
 		while (visited.size() < size)
 		{
 			/* Pick a node to use as a parent */
 			int index = RandomUtils.GENERATOR.nextInt(available.size());
 			Vector2i curLoc = available.get(index);
-
+			
 			/* Make sure we haven't visited this one */
 			if (visited.contains(curLoc)) continue;
-			
+
 			/* Compute the degree of this node */
 			int neighbors = 0;
 			for (Direction direction : Direction.toArray())
@@ -95,16 +95,16 @@ public class Polyomino
 				Vector2i newLoc = curLoc.add(vel);
 				if (visited.contains(newLoc)) neighbors++;
 			}
-			
+
 			/* Skip if there's no room to add a node */
 			if (neighbors >= Direction.numDirections())
 			{
 				available.remove(curLoc);
 				continue;
 			}
-
-			visited.add(curLoc);
 			
+			visited.add(curLoc);
+
 			/* Add the neighboring cells to the pool */
 			for (Direction direction : Direction.toArray())
 			{
@@ -113,47 +113,47 @@ public class Polyomino
 				available.add(newLoc);
 			}
 		}
-		
+
 		/* Final process over chosen locations */
 		for (Vector2i v : visited)
 		{
 			System.out.println("v = " + v);
-			
+
 			/* Build the block */
 			Block block = new Block();
-			
+
 			/* Make the block float */
 			block.setFloating(true);
-			
+
 			/* Now connect it to neighboring cells */
-			double internalConnectionChance = 0.3;
+			double internalConnectionChance = 0.25;
 			for (Direction direction : Direction.toArray())
 			{
 				Vector2i neighbor = v.add(direction.toVector());
 				if (visited.contains(neighbor))
 				{
 					boolean addWire = RandomUtils
-					        .randomDouble(internalConnectionChance);
+							.randomDouble(internalConnectionChance);
 					if (addWire) block.setWire(direction, true);
 				}
 			}
-			
+
 			/* Create the Cell instance for this Block instance */
 			Cell cell = new Cell(v, block);
 			m_cells.add(cell);
 		}
 	}
-
+	
 	public void removeFromGrid(Grid grid, GridPhysics gridPhysics)
 	{
 		updateInGrid(grid, gridPhysics, false);
 	}
-	
+
 	public void placeInGrid(Grid grid, GridPhysics gridPhysics)
 	{
 		updateInGrid(grid, gridPhysics, true);
 	}
-
+	
 	/*
 	 * Requires GridPhysics so we can incorporate gravity.
 	 */
@@ -183,56 +183,56 @@ public class Polyomino
 				break;
 		}
 		System.out.println("offset = " + m_offset);
-		
+
 		/* Find where the origin goes in the grid */
 		Vector2i center = getCenter(grid, gridPhysics);
-		
+
 		System.out.println("middle = " + middle);
 		System.out.println("center = " + center);
-		
+
 		/* Build and place each block */
 		for (Cell cell : m_cells)
 		{
 			/* Extract from cell */
 			Vector2i v = cell.getLoc();
 			Block block = present? cell.getBlock() : null;
-			
+
 			/* Compute where it will go */
 			Vector2i loc = middle.sub(center).add(v);
 			System.out.println("v = " + v + " ==> loc = " + loc);
-
+			
 			/* Place in the grid */
 			grid.setBlock(loc, block);
 		}
 	}
-	
+
 	public void translate(Grid grid, GridPhysics gridPhysics, int delta)
 	{
 		removeFromGrid(grid, gridPhysics);
 		m_offset += delta;
 		placeInGrid(grid, gridPhysics);
 	}
-	
+
 	public void rotate(Grid grid, GridPhysics gridPhysics, Rotation rotation)
 	{
 		/* Remove the blocks from the grid */
 		removeFromGrid(grid, gridPhysics);
-
+		
 		/* Now update all vectors */
 		for (Cell cell : m_cells)
 		{
 			Vector2i v = cell.getLoc();
 			v = rotation.rotateVector90(v);
 			cell.setLoc(v);
-
+			
 			Block block = cell.getBlock();
 			block.rotate(rotation);
 		}
-
+		
 		/* Put the blocks back into the grid */
 		placeInGrid(grid, gridPhysics);
 	}
-	
+
 	public void release()
 	{
 		for (Cell cell : m_cells)
@@ -241,7 +241,7 @@ public class Polyomino
 			block.setFloating(false);
 		}
 	}
-
+	
 	private Vector2i getMin()
 	{
 		/* We know for certain cell (0, 0) is in our set */
@@ -254,7 +254,7 @@ public class Polyomino
 		}
 		return ret;
 	}
-
+	
 	private Vector2i getMax()
 	{
 		/* We know for certain cell (0, 0) is in our set */
@@ -267,7 +267,7 @@ public class Polyomino
 		}
 		return ret;
 	}
-	
+
 	private Vector2i getCenter(Grid grid, GridPhysics gridPhysics)
 	{
 		/*
@@ -276,12 +276,12 @@ public class Polyomino
 		 */
 		Vector2i min = getMin();
 		Vector2i max = getMax();
-
+		
 		int x = 0;
 		int y = 0;
-
+		
 		Vector2i gravity = gridPhysics.getGravityVector();
-
+		
 		if (gravity.getY() < 0)
 		{
 			x = (min.getX() + max.getX()) / 2;
@@ -302,7 +302,7 @@ public class Polyomino
 			x = -Math.abs(min.getX());
 			y = (min.getY() + max.getY()) / 2;
 		}
-		
+
 		return new Vector2i(x, y);
 	}
 }
